1a5528b2a7a920e08ba848db69d96903b5e22938
alexvc1999 <alejandro99vicente@gmail.com>
2026-02-19 18:29:37 +0100

    Añado cambio de lenguaje

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,19 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { LanguageProvider } from "@/context/LanguageContext";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Prueba Tecnica",
-  description: "Alejandro Vicente",
-};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="es">
+      <body>
+        <LanguageProvider>{children}</LanguageProvider>
       </body>
     </html>
   );
 }
+export const metadata = {
+  title: "Prueba Tecnica - Alejandro",
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useLanguage } from "@/context/LanguageContext";
+
 export default function Navbar() {
   const router = useRouter();
+  const { language, setLanguage } = useLanguage();
 
   const logOut = () => {
     document.cookie =
       "auth-token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT";
     router.push("/login");
   };
+
   return (
     <nav className="flex justify-between px-10 py-5 bg-white border-b border-slate-200 sticky shadow-sm">
       <div className="flex items-center">
@@ -17,13 +21,36 @@ export default function Navbar() {
         </span>
       </div>
 
-      <div className="flex">
+      <div className="flex gap-5">
         <button
           onClick={logOut}
           className="px-5 py-2 text-m font-bold text-red-500 bg-red-50 hover:text-red-700 hover:bg-red-100 rounded-lg transition-all"
         >
-          Cerrar Sesión
+          {language === "es" ? "Cerrar Sesión" : "Logout"}
         </button>
+        <div className="flex bg-slate-100 p-1.5 rounded-xl border border-slate-200">
+          <button
+            onClick={() => setLanguage("es")}
+            className={`px-4 py-1.5 rounded-lg text-xs font-bold transition-all ${
+              language === "es"
+                ? "bg-white text-blue-600 shadow-sm"
+                : "text-slate-500 hover:text-slate-800"
+            }`}
+          >
+            ES
+          </button>
+
+          <button
+            onClick={() => setLanguage("en")}
+            className={`px-4 py-1.5 rounded-lg text-xs font-bold transition-all ${
+              language === "en"
+                ? "bg-white text-blue-600 shadow-sm"
+                : "text-slate-500 hover:text-slate-800"
+            }`}
+          >
+            EN
+          </button>
+        </div>
       </div>
     </nav>
   );
